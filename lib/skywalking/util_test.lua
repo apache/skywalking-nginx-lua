cd39379e5e2d9cc9f3f50cb00be25637fbf8b4f2
Wu Sheng <wu.sheng@foxmail.com>
2020-02-27 09:21:59 +0800

    Revert "feature: add CI and test cases for ngx lua. (#3)"
    This reverts commit ac4ba62db712fc20ff7422f31b267923419b0fb5.

--- lib/skywalking/util_test.lua
+++ lib/skywalking/util_test.lua
@@ -1,25 +1,25 @@
---
+-- 
 -- Licensed to the Apache Software Foundation (ASF) under one or more
 -- contributor license agreements.  See the NOTICE file distributed with
 -- this work for additional information regarding copyright ownership.
 -- The ASF licenses this file to You under the Apache License, Version 2.0
 -- (the "License"); you may not use this file except in compliance with
 -- the License.  You may obtain a copy of the License at
---
+-- 
 --    http://www.apache.org/licenses/LICENSE-2.0
---
+-- 
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
---
+-- 
 
 local lu = require('luaunit')
 local Util = require('util')
 
 TestUtil = {}
-    function TestUtil.testNewID()
+    function TestUtil:testNewID()
         local id = Util.newID()
 
         lu.assertNotNil(id[1])
@@ -27,7 +27,7 @@ TestUtil = {}
         lu.assertNotNil(id[3])
     end
 
-    function TestUtil.testTimestamp()
+    function TestUtil:testTimestamp()
         local id = Util.timestamp()
         lu.assertNotNil(id)
     end
