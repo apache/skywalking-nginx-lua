5ec9def2287580a7c0ad109d611b711d31d05bae
WenMing <moonbingbing@gmail.com>
2020-03-01 21:19:02 +0800

    fixed code style of register.

--- lib/skywalking/register.lua
+++ lib/skywalking/register.lua
@@ -1,28 +1,28 @@
--- 
+--
 -- Licensed to the Apache Software Foundation (ASF) under one or more
 -- contributor license agreements.  See the NOTICE file distributed with
 -- this work for additional information regarding copyright ownership.
 -- The ASF licenses this file to You under the Apache License, Version 2.0
 -- (the "License"); you may not use this file except in compliance with
 -- the License.  You may obtain a copy of the License at
--- 
+--
 --    http://www.apache.org/licenses/LICENSE-2.0
--- 
+--
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
--- 
+--
 
-local Register = {}
+local _M = {}
 
 -- Return Services as service register parameter
-function Register:newServiceRegister(unRegisterServiceName)
+function _M.newServiceRegister(unRegisterServiceName)
     local serv = {
         services = {}
     }
-    
+
     local service = {
         serviceName = unRegisterServiceName,
         -- Field type is optional, default value is `normal`
@@ -34,11 +34,11 @@ function Register:newServiceRegister(unRegisterServiceName)
     return serv
 end
 
-function Register:newServiceInstanceRegister(registeredServiceId, serviceInstUUID, registerTime)
+function _M.newServiceInstanceRegister(registeredServiceId, serviceInstUUID, registerTime)
     local serviceInstances = {
         instances = {}
     }
-    
+
     local serviceInstance = {
         serviceId = registeredServiceId,
         instanceUUID = serviceInstUUID,
@@ -53,7 +53,7 @@ function Register:newServiceInstanceRegister(registeredServiceId, serviceInstUUI
     return serviceInstances
 end
 
-function Register:newServiceInstancePingPkg(registeredServiceInstId, serviceInstUUID, updateTime)
+function _M.newServiceInstancePingPkg(registeredServiceInstId, serviceInstUUID, updateTime)
     local serviceInstancePingPkg = {
         serviceInstanceId = registeredServiceInstId,
         time = updateTime,
@@ -63,4 +63,4 @@ function Register:newServiceInstancePingPkg(registeredServiceInstId, serviceInst
     return serviceInstancePingPkg
 end
 
-return Register
+return _M
