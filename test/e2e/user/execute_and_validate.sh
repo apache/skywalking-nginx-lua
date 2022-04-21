9e374c645aad89e90ef57af4cf61293211f7336f
daming <zteny@foxmail.com>
2022-04-21 13:51:25 +0800

    support to try to use request-id as trace-id when trace context absent

--- test/e2e/user/execute_and_validate.sh
+++ test/e2e/user/execute_and_validate.sh
@@ -15,35 +15,72 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+trigger() {
+  url=$(pos=$1 yq '.entries.[env(pos)].url' ./scenario)
+  method=$(pos=$1 yq '.entries.[env(pos)].method // "GET"' ./scenario)
 
-rst=$(curl -X POST -Is ${SERVICE_ENTRY} | head -1 | grep "HTTP/1.1 200")
-if [[ -z "$rst" ]]; then
-  echo "failed to access ${SERVICE_ENTRY}"
-  exit 1
-fi
-echo "access ${SERVICE_ENTRY} success"
+  pos=$1 yq '.entries.[env(pos)].headers.[]' ./scenario > /req_header
 
-rst=$(curl -X GET -Is ${SUFFIX_ENTRY} | head -1 | grep "HTTP/1.1 200")
-if [[ -z "$rst" ]]; then
-  echo "failed to access ${SUFFIX_ENTRY}"
-  exit 1
-fi
-echo "access ${SUFFIX_ENTRY} success"
-
-sleep 5 # Wait Agent reported TraceSegment.
-
-times=0
-while [ $times -lt $MAX_RETRY_TIMES ]; do
-  curl -X POST --data-raw "$(cat /expectedData.yaml)" --dump-header ./header -o /response -s ${VALIDATION_ENTRY}
-  rst=$(head -1 /header | grep "HTTP/1.1 200")
-  if [[ -n "$rst" ]]; then
-    echo "Verification successful"
-    exit 0
+  rst=$(curl -Is -X $method -H @/req_header $url | head -1 | grep "HTTP/1.1 200")
+  if [[ -z "$rst" ]]; then
+    echo "failed to access $2"
+    exit 1
   fi
+  echo "access $2 successful"
+}
 
-  sleep 3
-  times=$((times+1))
+clear() {
+  rst=$(curl -Is -X GET http://${COLLECTOR}/receiveData/clear | head -1 | grep "HTTP/1.1 200")
+  if [[ -z "$rst" ]]; then
+    echo "failed to clear collector segments"
+    exit 1
+  fi
+  echo "sweep collector segments successful"
+}
+
+validate() {
+  name=$1
+  expectedData=$2
+
+  times=0
+  while [ $times -lt $MAX_RETRY_TIMES ]; do
+    curl -X POST --data-raw "$(cat $expectedData)" --dump-header ./header -o /response -s ${COLLECTOR}/dataValidate
+    rst=$(head -1 /header | grep "HTTP/1.1 200")
+    if [[ -n "$rst" ]]; then
+      echo "scenario $name verification successful"
+      return 0
+    fi
+
+    sleep 3
+    times=$((times+1))
+  done
+
+  cat /response
+  echo "scenario $name verification failed"
+  exit 1
+}
+
+scenarios=$(yq e '.scenarios | length' /config.yaml)
+echo "total scenarios number: $scenarios"
+
+scenario=0
+while [ $scenario -lt $scenarios ]; do
+  clear
+
+  pos=$scenario yq -P '.scenarios.[env(pos)]' /config.yaml > ./scenario
+  name=$(yq '.name' ./scenario)
+  entries=$(yq '.entries | length' ./scenario)
+  expectedData=$(yq '.expected' ./scenario)
+
+  entry=0
+  while [ $entry -lt $entries ]; do
+    trigger $entry
+
+    entry=$((entry+1))
+  done
+
+  sleep 5 # wait for agent report trace segments.
+  validate $name $expectedData
+
+  scenario=$((scenario+1))
 done
-
-cat /response
-exit 1
